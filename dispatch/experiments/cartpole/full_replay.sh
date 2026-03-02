94546953172cf95d1129ae2e36d0d93a650160e1
Sebastian Pusch <seba0808@gmail.com>
2026-03-02 13:11:42 +0100

    move time arg

--- dispatch/experiments/cartpole/full_replay.sh
+++ dispatch/experiments/cartpole/full_replay.sh
@@ -1,17 +1,17 @@
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 1 --project cartpole_full_replay --method sequential --name_prefix sequential-s1
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 2 --project cartpole_full_replay --method sequential --name_prefix sequential-s2
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 3 --project cartpole_full_replay --method sequential --name_prefix sequential-s3
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 4 --project cartpole_full_replay --method sequential --name_prefix sequential-s4
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 5 --project cartpole_full_replay --method sequential --name_prefix sequential-s5
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 1 --project cartpole_full_replay --method sequential --name_prefix sequential-s1
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 2 --project cartpole_full_replay --method sequential --name_prefix sequential-s2
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 3 --project cartpole_full_replay --method sequential --name_prefix sequential-s3
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 4 --project cartpole_full_replay --method sequential --name_prefix sequential-s4
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 5 --project cartpole_full_replay --method sequential --name_prefix sequential-s5
 
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 1 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s1
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 2 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s2
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 5 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s5
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 3 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s3
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 4 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s4
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 1 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s1
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 2 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s2
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 5 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s5
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 3 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s3
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 4 --project cartpole_full_replay --method fine_tune --name_prefix fine_tune-s4
 
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 1 --project cartpole_full_replay --method continual --name_prefix continual-s1
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 2 --project cartpole_full_replay --method continual --name_prefix continual-s2
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 3 --project cartpole_full_replay --method continual --name_prefix continual-s3
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 4 --project cartpole_full_replay --method continual --name_prefix continual-s4
-sbatch dispatch/dispatch_cartpole.sh --time=00:30:00 --seed 5 --project cartpole_full_replay --method continual --name_prefix continual-s5
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 1 --project cartpole_full_replay --method continual --name_prefix continual-s1
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 2 --project cartpole_full_replay --method continual --name_prefix continual-s2
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 3 --project cartpole_full_replay --method continual --name_prefix continual-s3
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 4 --project cartpole_full_replay --method continual --name_prefix continual-s4
+sbatch --time=00:30:00 dispatch/dispatch_cartpole.sh --seed 5 --project cartpole_full_replay --method continual --name_prefix continual-s5
