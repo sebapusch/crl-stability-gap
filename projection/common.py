d949f407d10ad632eefc3b86ca40ccb4b0e3274a
Sebastian Pusch <seba0808@gmail.com>
2026-04-10 15:03:51 +0200

    make project directory

--- projection/common.py
+++ projection/common.py
@@ -1,13 +1,16 @@
-import os
 import sys
+from os import mkdir, path
 
 from stable_baselines3.common.logger import Logger, HumanOutputFormat, WandbWriter, CSVOutputFormat
 
 
 def make_logger(project: str, run_name: str) -> Logger:
-    csv_path = os.path.abspath(os.path.join(
-        __file__, '..', '..', 'output', project, f'{run_name}.csv')
-    )
+    dir_path = path.join(__file__, '..', '..', 'output', project)
+
+    if not path.exists(dir_path):
+        mkdir(dir_path)
+
+    csv_path = path.abspath(path.join(dir_path, f'{run_name}.csv'))
 
     return Logger(
         folder='../.logs',
